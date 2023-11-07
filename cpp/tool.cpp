8320233559972a99fe3e06cf888310b158b1e8b7
Yartsev Vladimir <v.rainovsky@yandex.ru>
2023-11-07 12:29:01 +0300

    added tool functions

--- cpp/tool.cpp
+++ cpp/tool.cpp
@@ -26,3 +26,46 @@ void Tool::setToolType(ToolType newToolType)
 {
     m_toolType = newToolType;
 }
+
+QByteArray Tool::move(int x, int y, int z)
+{
+    return QString("GO X%1 Y%2 Z%3")
+            .arg(x)
+            .arg(y)
+            .arg(z)
+            .toLatin1();
+}
+
+QByteArray Tool::moveAndExtrude(double x, double y, double z, double extrudeVolume)
+{
+    return QString("G1 X%1 Y%2 Z%3 E%4")
+            .arg(x)
+            .arg(y)
+            .arg(z)
+            .arg(extrudeVolume)
+            .toLatin1();
+}
+
+QByteArray Tool::pauseSec(int seconds)
+{
+    return QString("G4 S%1")
+            .arg(seconds)
+            .toLatin1();
+}
+
+QByteArray Tool::paudeMillsec(int millsec)
+{
+    return QString("G4 P%1")
+            .arg(millsec)
+            .toLatin1();
+}
+
+QByteArray Tool::headUp(int mm)
+{
+    return move(0, 0, mm);
+}
+
+QByteArray Tool::headDown(int mm)
+{
+    return move(0, 0, -mm);
+}
--- include/tool.h
+++ include/tool.h
@@ -1,13 +1,14 @@
 #ifndef TOOL_H
 #define TOOL_H
 
+#include <QString>
 
 class Tool
 {
 public:
+
     enum ToolType{
-        Pen,
-        Laser
+        Extruder
     };
 
     Tool(ToolType newToolType, int newFeedRate);
@@ -19,9 +20,17 @@ public:
     int toolType() const;
     void setToolType(ToolType newToolType);
 
+    QByteArray move(int x, int y, int z);
+    QByteArray moveAndExtrude(double x, double y, double z, double extrudeVolume);
+    QByteArray pauseSec(int seconds);
+    QByteArray paudeMillsec(int millsec);
+
+    QByteArray headUp(int mm);
+    QByteArray headDown(int mm);
 protected:
     int m_feedRate;
     int m_toolType;
+
 };
 
 #endif // TOOL_H
